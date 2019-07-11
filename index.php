df6e7832fb797bc549a3b8cfa68137939fd04ab6
damian <damian@damian.id.au>
2019-07-11 22:44:53 +1000

    Started working on basic template.

---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+    <head>
+        <title>Zendesk Scheduler</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.side-nav {
+  height: 100%;
+  width: 60px;
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  background-color: #002144;
+}
+
+.side-nav a {
+  padding: 17px 17px 17px 17px;
+  color: #7F90A1;
+  display: block;
+  align: center;
+  width: 60px;
+}
+
+.side-nav a:hover {
+  color: #f1f1f1;
+}
+
+.top-nav {
+    height: 55px;
+    width: 100%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #F8F9F9;
+    padding-top: 13px;
+    padding-right: 13px;
+}
+
+.main {
+    height: 100%;
+    padding-left: 80px;
+    padding-top: 60px;
+}
+</style>
+</head>
+<body>
+
+<div class="side-nav">
+    <img src="images/logo.png" style="padding: 17px;"></imsg>
+    <a href="#"><i class="fa fa-home fa-2x"></i></a>
+    <a href="#"><i class="fa fa-th-list fa-2x"></i></a>
+    <a href="#"><i class="fa fa-cog fa-2x"></i></a>
+</div>
+
+<div class="top-nav">
+    <div class="dropdown">
+        <p class="text-right"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user-circle fa-2x"></i></a></p>
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+  <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+            <li><a href="#">HTML</a></li>
+            <li><a href="#">CSS</a></li>
+            <li><a href="#">JavaScript</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="main">
+    <h3>Zendesk Scheduler</h3>
+    A piece of content will go here probably.
+    
+    <div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="#">HTML</a></li>
+    <li><a href="#">CSS</a></li>
+    <li><a href="#">JavaScript</a></li>
+  </ul>
+</div>
+</div>
+
+<script>
+</script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   
+</body>
+</html> 
